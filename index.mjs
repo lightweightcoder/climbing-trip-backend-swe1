f5e124513de0f846806813b8486418092f870b54
aljt <alvinljt@gmail.com>
2021-02-18 22:44:16 +0800

    added express middleware to parse JSON request bodies

--- index.mjs
+++ index.mjs
@@ -18,6 +18,8 @@ app.use(cors({
 app.use(cookieParser());
 // Bind Express middleware to parse request bodies for POST requests
 app.use(express.urlencoded({ extended: false }));
+// Bind Express middleware to parse JSON request bodies
+app.use(express.json());
 // Bind method override middleware to parse PUT and DELETE requests sent as POST requests
 app.use(methodOverride('_method'));
 // Expose the files stored in the public folder
